999d672d67bc55d40b419eca7c8decbd85e4a631
Alexiiisv <56995052+Alexiiisv@users.noreply.github.com>
2021-10-28 21:28:52 +0200

    Create 'Pion' and 'Dame' Class

---
+++ src/meilleur/com/model/Pion.java
@@ -0,0 +1,14 @@
+package meilleur.com.model;
+
+public class Pion {
+
+    private Character Letter; //j1 = 'p' || j2 = 'P'
+
+    public Character getLetter() {
+        return Letter;
+    }
+
+    public void setLetter(Character letter) {
+        Letter = letter;
+    }
+}
