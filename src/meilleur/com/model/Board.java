a020e4f794ae329f29f5dd04b6ce1a5e4a9273fa
BoSswosile <yohan.moren@gmail.com>
2021-10-28 22:08:17 +0200

    Starting Board Function

--- src/meilleur/com/model/Board.java
+++ src/meilleur/com/model/Board.java
@@ -1,5 +1,23 @@
 package meilleur.com.model;
 
+import java.util.Arrays;
+
 public class Board {
+    private int numb = 0;
+    private int L = 10;
+    private int H = 10;
+    private char[][] map = new char[L][H];
+    public void CreateBoard(){
+        for (char[] col : map) {
+            Arrays.fill(col, '.');
+            }
+        }
+
+    public void printMap() {
+        for (char[] s : map) System.out.println(Arrays.toString(s));
+        System.out.println();
+    }
 
 }
+
+
--- src/meilleur/com/utilitaire/Game.java
+++ src/meilleur/com/utilitaire/Game.java
@@ -1,9 +1,13 @@
 package meilleur.com.utilitaire;
 
+import meilleur.com.model.Board;
+
 public class Game {
 
     public void newGame() {
-        System.out.println("TP - Échecs");
-
+        System.out.println("TP - Échecs \n");
+        Board board = new Board();
+        board.CreateBoard();
+        board.printMap();
     }
 }
