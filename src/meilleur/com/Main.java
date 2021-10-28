fd700db89008710fb5b7d4bcd71395a857f8dc8e
Alexiiisv <alvelleine@gmail.com>
2021-10-28 15:53:33 +0200

    first commit

---
+++ src/meilleur/com/Main.java
@@ -0,0 +1,7 @@
+package meilleur.com;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("TP - Échecs");
+    }
+}
---
+++ src/meilleur/com/utilitaire/Function.java
@@ -0,0 +1,4 @@
+package meilleur.com.utilitaire;
+
+public class Function {
+}
