b63f9feb1a06a44ca41a8fa624b7d3485b49ff68
BoSswosile <yohan.moren@gmail.com>
2021-10-29 19:36:59 +0200

    Starting Board Function

--- src/meilleur/com/model/Board.java
+++ src/meilleur/com/model/Board.java
@@ -7,18 +7,21 @@ public class Board {
     private final int L = 10;
     private final int H = 10;
     private char[][] map = new char[L][H];
+    private int testing = 1;
+    private int testink = 1;
 
-    public void createBoard(){
+    public void createBoard() {
         for (char[] col : map) {
             Arrays.fill(col, '.');
-            }
         }
+    }
 
     public void printMap() {
         for (char[] s : map) System.out.println(Arrays.toString(s));
         System.out.println();
     }
-
 }
+
+
 
 
